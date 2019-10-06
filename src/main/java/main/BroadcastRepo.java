14315e68f9815adc307e2d6952e30319f0e2e417
Lucian Cristian <cristianlucian84@gmail.com>
2019-10-07 00:41:52 +0300

    fixed the problem of selling a ticket to multiple same dates

--- src/main/java/main/BroadcastRepo.java
+++ src/main/java/main/BroadcastRepo.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,5 +12,5 @@ import db.Broadcast;
 public interface BroadcastRepo extends JpaRepository<Broadcast, Integer>{
 	
 	@Query("FROM Broadcast b where date = :date")
-	public Broadcast getBroadcastByDate(@Param("date") LocalDateTime date);
+	public List<Broadcast> getBroadcastByDate(@Param("date") LocalDateTime date);
 }
--- src/main/java/main/ClientController.java
+++ src/main/java/main/ClientController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 import java.time.LocalDateTime;
 
 import db.Broadcast;
@@ -42,23 +43,31 @@ public class ClientController {
 	@PostMapping("sell")
 	public String addClient(Model m, @ModelAttribute Client client, @ModelAttribute Contact contact, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") LocalDateTime date)
 	{
-		Broadcast broadcast = broadcastRepo.getBroadcastByDate(date);
+		List<Broadcast> broadcast = broadcastRepo.getBroadcastByDate(date);
 		
 		/**
 		 * verify if there are available seats for that date
 		 */
-		if (broadcast.getRemainingSeats() > 0) {
-			m.addAttribute(broadcast);
-			client.setContact(contact);
-			client.setBroadcast(broadcast);
-			clientRepo.save(client);
-			broadcast.setRemainingSeats(broadcast.getRemainingSeats()-1);
-			broadcastRepo.save(broadcast);
-			return "resultAddClient";
-		}
-		else {
-			return "resultNoMoreSeats";
+		int maxValue = broadcast.size();
+		int index;
+		for (index=0; index < maxValue; index++) {
+			if (broadcast.get(index).getRemainingSeats() > 0) {
+				break;
+			}
 		}
 		
+		if (index == broadcast.size()) {
+			return "resultNoMoreSeats"; }
+		
+		if (broadcast.get(index).getRemainingSeats() > 0) {
+			m.addAttribute(broadcast.get(index));
+			client.setContact(contact);
+			client.setBroadcast(broadcast.get(index));
+			clientRepo.save(client);
+			broadcast.get(index).setRemainingSeats(broadcast.get(index).getRemainingSeats()-1);
+			broadcastRepo.save(broadcast.get(index));
+			return "resultAddClient";
+		}
+		return "resultNoMoreSeats";
 	}
 }
